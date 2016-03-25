266d852950993c1d81aa0a9476ebae85f85b811e
Chris <chris@chrisob.com>
2016-03-25 15:12:09 -0700

    add test bootstrap

---
+++ tests/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+error_reporting(E_ALL);
+
+// WoWApi autoload
+require_once(realpath(dirname(__FILE__) . '/../') . "/src/WowApi/autoload.php");
+
+// composer autoload
+require_once(realpath(dirname(__FILE__) . '/../') . "/vendor/autoload.php");
