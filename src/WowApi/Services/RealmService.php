2891ffb12c5e424944a30266448abde568ab9427
Chris <chris@chrisob.com>
2016-04-08 00:45:21 -0700

    update RealmService with new interface methods

--- src/WowApi/Services/RealmService.php
+++ src/WowApi/Services/RealmService.php
@@ -17,54 +17,56 @@ use WowApi\Util\Helper;
 class RealmService extends BaseService {
 
     /**
-     * @var array $ruleSets
+     * Realm type constants
      */
-    public $ruleSets;
+    const TYPE_PVE = 'pve';
+    const TYPE_PVP = 'pvp';
+    const TYPE_RP = 'rp';
+    const TYPE_RPPVP = 'rppvp';
 
     /**
-     * @var array $populationTypes
+     * Queue constants
      */
-    public $populationTypes;
+    const QUEUE_YES = true;
+    const QUEUE_NO = false;
 
     /**
-     * RealmService constructor
-     *
-     * @param string $apiKey
-     * @param array|null $options
+     * Server status constants
      */
-    public function __construct($apiKey, $options = null) {
-        parent::__construct($apiKey, $options);
-
-        $this->ruleSets = ['pve', 'pvp', 'rp', 'rppvp'];
-        $this->populationTypes = ['high', 'medium', 'low'];
-    }
+    const STATUS_UP = true;
+    const STATUS_DOWN = false;
 
     /**
-     * Get Realm component
-     *
-     * @param string $realm Realm slug
-     * @return Realm
-     * @throws WowApiException
-     * @throws NotFoundException
+     * Server population constants
      */
-    public function getRealm($realm) {
-        $this->setQuery(['realm' => Helper::formatSlug($realm)]);
+    const POP_LOW = 'low';
+    const POP_MEDIUM = 'medium';
+    const POP_HIGH = 'high';
 
-        $request = parent::createRequest('GET', 'realm/status');
-
-        try {
-            $response = parent::doRequest($request);
-        } catch (ClientException $e) {
-            throw parent::toWowApiException($e);
-        }
-
-        // if there is more than one result from a getRealm request, that means it was not found. the API returns all realms if the query string search does not find one
-        if (count(json_decode($response->getBody())->realms) > 1) {
-            throw parent::toWowApiException(['Realm Not Found', 404]);
-        } else {
-            return new Realm($response->getBody());
-        }
-    }
+    /**
+     * @var array $schema
+     */
+    protected $schema = [
+        'type' => [
+            self::TYPE_PVE,
+            self::TYPE_PVP,
+            self::TYPE_RP,
+            self::TYPE_RPPVP
+        ],
+        'queue' => [
+            self::QUEUE_YES,
+            self::QUEUE_NO
+        ],
+        'status' => [
+            self::STATUS_UP,
+            self::STATUS_DOWN
+        ],
+        'population' => [
+            self::POP_LOW,
+            self::POP_MEDIUM,
+            self::POP_HIGH
+        ],
+    ];
 
     /**
      * Gets the realm(s)
@@ -74,7 +76,7 @@ class RealmService extends BaseService {
      * @throws WowApiException
      * @throws IllegalArgumentException
      */
-    public function getRealms($realms = null) {
+    public function get($realms = null) {
         $error = false;
 
         // realms check
@@ -107,16 +109,59 @@ class RealmService extends BaseService {
     }
 
     /**
-     * Sort realms by given key val pair
+     * Get Realm component
      *
+     * @param string $realm Realm slug
+     * @return Realm
+     * @throws WowApiException
+     * @throws NotFoundException
+     */
+    public function find($realm) {
+        $this->setQuery(['realm' => Helper::formatSlug($realm)]);
+
+        $request = parent::createRequest('GET', 'realm/status');
+
+        try {
+            $response = parent::doRequest($request);
+        } catch (ClientException $e) {
+            throw parent::toWowApiException($e);
+        }
+
+        // if there is more than one result from a getRealm request, that means it was not found. the API returns all realms if the query string search does not find one
+        if (count(json_decode($response->getBody())->realms) > 1) {
+            throw parent::toWowApiException(['Realm Not Found', 404]);
+        } else {
+            return new Realm($response->getBody());
+        }
+    }
+
+    /**
      * @param string $key
      * @param string $val
      * @return array
+     * @throws IllegalArgumentException
      */
-    public function sortRealms($key, $val) {
-        $this->sortWhitelist = ['type', 'population', 'status'];
-        
-        return $this->sortData($this->getRealms(), [$key => $val]);
+    public function filter($key, $val) {
+        if (!in_array($val, $this->schema[$key])) {
+            throw new IllegalArgumentException();
+        }
+
+        $this->sortWhitelist = ['type', 'queue', 'status', 'population'];
+        return $this->sortData($this->get(), [$key => $val]);
+    }
+
+    /**
+     *
+     */
+    public function getDownRealms() {
+        $this->filter('status', self::STATUS_DOWN);
+    }
+
+    /**
+     *
+     */
+    public function getRealmsWithQueue() {
+        $this->filter('queue', self::QUEUE_YES);
     }
 
 }
